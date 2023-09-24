9e27fa5597f4e3dd6ef380b5d60710db718243a8
mariogazziro <84649066+mariogazziro@users.noreply.github.com>
2023-09-24 03:40:27 -0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,53 +1,48 @@
 #include "neslib.h"
 //#link "chr_generic.s"
-#define MAXITERATE 16
-#define NORM_BITS 13
-#define NORM_FACT ((long) 1 << NORM_BITS)
-
-int iterate(long real0, long imag0)
-{
+#define MAXITERATE 16 //numero maximo de iteracoes
+#define NORM_BITS 13 //fator de normalizacao dos reais para inteiros
+#define NORM ((long) 1 << NORM_BITS) //fator de 13 bits de normalizacao
+int iterate(long real0, long imag0) {
    long realq, imagq, real, imag;
    int i;
    real = real0;
    imag = imag0;
    for (i = 0; i < MAXITERATE; i++) {
-     realq = (real * real) >> NORM_BITS;
-     imagq = (imag * imag) >> NORM_BITS;
-     if ((realq + imagq) > 4 * NORM_FACT)
-        break;
-     imag = ((real * imag) >> (NORM_BITS - 1)) + imag0;
-     real = realq - imagq + real0;
+     realq = (real * real) >> NORM_BITS; //elevando ao quadrado...
+     imagq = (imag * imag) >> NORM_BITS; //...o ultimo elemento.
+     if ((realq + imagq) > 4 * NORM)
+        break;   //(se o valor exceder o limiar, sair do funcao) 
+     imag = ((real * imag) >> (NORM_BITS - 1)) + imag0; //adicionando...
+     real = realq - imagq + real0; //...seu numero inicial
    }
    return i;
 }
-
-void mand_calc(long realmin, long imagmin, long realmax, long imagmax, long hres, long vres)
+void mand_calc(long realmin, long imagmin, long realmax, long imagmax, long hres, long vres) 
 {
-  int deltareal, deltaimag, real0,  imag0;
-  int x, y, z;
-  deltareal = (realmax - realmin) / hres;
-  deltaimag = (imagmax - imagmin) / vres;
+  int deltareal, deltaimag, real0,  imag0, x, y, z;
+  deltareal = (realmax - realmin) / hres; //define o passo em funcao...
+  deltaimag = (imagmax - imagmin) / vres; //...da resolucao do fractal
   real0 = realmin;
-  for (y = 1; y <= vres; y++) {
+  for (y = 1; y <= vres; y++) { //varredura na resolucao...
     imag0 = imagmax;
-    for (x = 1; x <= hres; x++) {
+    for (x = 1; x <= hres; x++) { //...dos blocos, 32x30 ao todo
       z=iterate(real0, imag0);
-      if (z>=MAXITERATE) {
-        ppu_off();
-        vram_adr(NTADR_A(x,y));   
-        vram_put(1);
-        ppu_on_bg();
+      if (z>=MAXITERATE) {//se excedeu o limiar, insirir um bloco escuro
+        ppu_off(); //desliga o controlador grafico antes de escrever
+        vram_adr(NTADR_A(x,y)); //ajusta as coordenadas na tela 
+        vram_put(1); //desenha o bloco escuro nas coordenadas
+        ppu_on_bg(); //liga o controlador grafico novamente e exibe tela
       }
-      imag0 = imag0 - deltaimag;
+      imag0 = imag0 - deltaimag; //atualiza os indices complexos...
     }
-    real0 = real0 + deltareal;
+    real0 = real0 + deltareal; // ...para a proxima iteracao
   }
 }
-
 void main(void) {
-  long int bbox[]={-2,-1,1,1}; //realmin, imagmin, realmax, imagmax
-  int width = 32, height = 30; // pixel resolution
-  pal_col(0,0xA); // set screen to light green  
-  mand_calc(bbox[0]*NORM_FACT, bbox[1]*NORM_FACT, bbox[2]*NORM_FACT, bbox[3]*NORM_FACT, width, height);
-  while (1); // infinite loop
+  long C[]={-2,-1,1,1}; //coordenadas da janela no plano complexo
+  int width = 32, height = 30; //resolucao do fractal em pixels
+  pal_col(0,0xA); //ajusta a tela de fundo para verde escuro    
+  mand_calc(C[0]*NORM,C[1]*NORM,C[2]*NORM,C[3]*NORM,width,height);
+  while (1); //a funcao acima multiplica os parametros reais pela norma
 }
